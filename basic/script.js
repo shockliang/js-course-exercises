2674afe49b5c39d963358ef3cf622f315880f82c
Shock Liang <shockwater@gmail.com>
2017-07-03 21:09:58 +0800

    Add start file for section 2

--- basic/script.js
+++ basic/script.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+
+// object and method
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() {
+        return 2016 - this.yearOfBirth;
+    }
+};
