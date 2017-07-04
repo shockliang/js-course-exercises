e844ba1a1faa5db857b0e0bc051d846acbe669a6
Shock Liang <shockwater@gmail.com>
2017-07-04 22:27:29 +0800

    Events and event handling: rolling the dice

--- dom-pig-game/app.js
+++ dom-pig-game/app.js
@@ -12,14 +12,24 @@ GAME RULES:
 var scores = [0, 0];
 var roundScore = 0;
 var activePlayer = 0;
-var dice = Math.floor(Math.random() * 6) + 1;
 
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+
+});
