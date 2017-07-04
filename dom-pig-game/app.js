311b92f714bc91eb766df48cc22068929d4302e1
Shock Liang <shockwater@gmail.com>
2017-07-04 23:14:45 +0800

    Create game initial function

--- dom-pig-game/app.js
+++ dom-pig-game/app.js
@@ -9,17 +9,9 @@ GAME RULES:
 
 */
 
-var scores = [0, 0];
-var roundScore = 0;
-var activePlayer = 0;
+var scores, roundScore, activePlayer;
 
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+initialGame();
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -70,4 +62,26 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
     
     document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', initialGame);
+
+function initialGame() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
 }
