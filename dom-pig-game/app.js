dccd3af4c29f48e63e97eeb76a30c66688e273b2
Shock Liang <shockwater@gmail.com>
2017-07-04 23:01:42 +0800

    Implement hold function

--- dom-pig-game/app.js
+++ dom-pig-game/app.js
@@ -23,6 +23,7 @@ document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.dice').style.display = 'none';
 
+// Rolling the dice
 document.querySelector('.btn-roll').addEventListener('click', function() {
     var dice = Math.floor(Math.random() * 6) + 1;
 
@@ -35,17 +36,38 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        // next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 
 });
+
+// Hold the scores
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    // Add current socres to global score
+    scores[activePlayer] += roundScore;
+
+    document.querySelector('#score-' + activePlayer ).textContent = scores[activePlayer];
+
+    // Check if player won the game
+    if(scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
+    document.querySelector('.dice').style.display = 'none';
+}
