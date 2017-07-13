097b3368ef4c5592ed30c6a664bca8e6e1ed252d
Shock Liang <shockwater@gmail.com>
2017-07-13 22:21:06 +0800

    Reading the input data

--- budget-app/app.js
+++ budget-app/app.js
@@ -4,13 +4,37 @@ var budgetController = (function () {
 })();
 
 var UIController = (function() {
+    
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description', 
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
 
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+
+        getDOMStrings: function() {
+            return DOMStrings;
+        }
+    }
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem = function() {
         // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget.
@@ -19,7 +43,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         console.log('ctrlAddItem');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
