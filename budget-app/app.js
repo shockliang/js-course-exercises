dc207aca56085fae51cbdac2800bd5604394c244
Shock Liang <shockwater@gmail.com>
2017-07-16 16:35:45 +0800

    Clear the input fields

--- budget-app/app.js
+++ budget-app/app.js
@@ -96,6 +96,19 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
         },
 
+        clearFields: function() {
+            var fields, fieldsArray;
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue);
+
+            var fieldsArray = Array.prototype.slice.call(fields);
+            
+            fieldsArray.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArray[0].focus();
+        },
+
         getDOMStrings: function() {
             return DOMStrings;
         }
@@ -123,15 +136,16 @@ var controller = (function (budgetCtrl, UICtrl) {
         
         // 2. Add the item to the budget controller
         var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        console.log(newItem);
-        // budgetCtrl.testing();
 
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        // 4. Calculate the budget.
-        
-        // 5. Display the budget on the UI.
+        // 4. Clear the fields
+        UICtrl.clearFields();
+
+        // 5. Calculate the budget.
+
+        // 6. Display the budget on the UI.
 
         console.log('ctrlAddItem');
     };
