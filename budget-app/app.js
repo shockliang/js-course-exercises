b37ffcc8385830ba9c4d582b03ddd84cea0781be
Shock Liang <shockwater@gmail.com>
2017-07-16 16:54:54 +0800

    Update the budget controller

--- budget-app/app.js
+++ budget-app/app.js
@@ -72,7 +72,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMStrings.inputType).value,
                 description: document.querySelector(DOMStrings.inputDescription).value,
-                value: document.querySelector(DOMStrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMStrings.inputValue).value)
             }
         },
 
@@ -129,25 +129,33 @@ var controller = (function (budgetCtrl, UICtrl) {
             }
         });
     };
+    
+    var updateBudget = function() {
+        // 1. Calculate the budget.
+
+        // 2. Return the budget.
+
+        // 3. Display the budget on the UI.
+    };
 
     var ctrlAddItem = function() {
         // 1. Get the filed input data
         var input = UICtrl.getInput();
         
-        // 2. Add the item to the budget controller
-        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            budgetCtrl.testing();
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 4. Clear the fields
+            UICtrl.clearFields();
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
-
-        // 5. Calculate the budget.
-
-        // 6. Display the budget on the UI.
-
-        console.log('ctrlAddItem');
+            // 5. Calculate and update budget.
+            updateBudget();
+        }
+        
     };
 
     return {
